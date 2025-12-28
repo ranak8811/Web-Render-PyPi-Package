eb45b5672aa582534c825290d0350c7805f794b5
Anwar_Hossain <ranakrphone@gmail.com>
2025-12-28 15:23:22 +0600

    web redner function added

---
+++ src/WebRender/youtube.py
@@ -0,0 +1,38 @@
+from WebRender.custom_execution import InvalidURLException
+from WebRender.logger import logger
+import re
+from IPython.display import HTML, display
+
+def render_youtube_video(url: str, width: int = 760, height: int = 440):
+    try:
+        regex = r"(?:v=|\/)([0-9A-Za-z_-]{11})"
+        match = re.search(regex, url)
+        
+        if not match:
+            raise InvalidURLException('Provided URL is invalid', url)
+        
+        video_id = match.group(1)
+
+        embed_url = f"https://www.youtube-nocookie.com/embed/{video_id}"
+
+
+        iframe = f"""
+        <iframe width="{width}" height="{height}" 
+        src="{embed_url}" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        referrerpolicy="strict-origin-when-cross-origin" 
+        allowfullscreen>
+        </iframe>
+        """
+
+        display(HTML(iframe))
+
+        logger.info("Video rendered successfully")
+
+        return 'success'
+
+    except Exception as e:
+        logger.error(f'Caught Exception: {e}')
+        raise InvalidURLException
