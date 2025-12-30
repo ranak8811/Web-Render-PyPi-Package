c738e4e6c253be14a13e05cb8152ed2cc3c638d7
Anwar_Hossain <ranakrphone@gmail.com>
2025-12-30 13:56:51 +0600

    updated the version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 REPO_NAME = "Web-Render-PyPi-Package"
 AUTHOR_USER_NAME = "ranak8811"
@@ -17,7 +17,7 @@ setuptools.setup(
     author_email=AUTHOR_EMAIL,
     description="A small web render python package",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
